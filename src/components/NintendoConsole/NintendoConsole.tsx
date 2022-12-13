a235f6383e727d9690fb1780f8f8a179eeda1b87
genjutsu <andresd.ad5@gmail.com>
2022-12-13 14:46:00 -0300

    intefaz add

--- src/components/NintendoConsole/NintendoConsole.tsx
+++ src/components/NintendoConsole/NintendoConsole.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import {useState} from 'react'
+import { render } from 'react-dom';
 import {AiOutlineArrowUp,AiOutlineArrowRight,AiOutlineArrowLeft,AiOutlineArrowDown} from 'react-icons/ai' 
 import SonidoTecla from '../../assets/boton.mp3'
+import ScreenInit from '../ScreenInit/ScreenInit';
 export default function NintendoConsole() {
+    const [renderInitGame, setRenderInitGame] = useState<boolean>(false)
+    const [renderGame, setRenderGame] = useState<boolean>(false)
     const sound = new Audio(SonidoTecla);
+    const start = ()=>{
+        if(renderInitGame){
+            sound.play();
+            setRenderGame(true)
+        }
+    }
   return (
     <div className='bg-slate-300  w-screen  sm:w-2/4 h-screen rounded-xl sm:m-auto' style={{borderBottomRightRadius:"6rem"}}>
         <div className=' h-6 m-auto w-2/3 border-b-2   border-b-slate-200 '></div>
-        <div className=' bg-slate-500 rounded w-5/6  mt-5 sm:w-96 sm:h-80 m-auto flex flex-col' style={{borderBottomRightRadius:"4rem"}}>
+        <div className=' bg-slate-500 rounded w-5/6  mt-4 sm:w-96 sm:h-80 m-auto flex flex-col' style={{borderBottomRightRadius:"4rem"}}>
             <div className='  flex items-center justify-between'><span className='ml-3 w-12 sm:w-28 h-3 border-y-2 border-b-blue-700 border-t-rose-500'></span><p className=' text-xs text-white pt-2 pb-2'>DOT MATRIX WITH STEREO SOUND</p><span className=' w-7 sm:w-12 h-3 border-y-2 border-b-blue-700 border-t-rose-500 mr-3'></span></div>
-                <div className=' bg-lime-500 w-60 h-60 sm:w-64 sm:h-64 mx-auto my-auto mb-7' >       
-            </div>
+              {!renderInitGame ? <div className='bg-lime-500 w-60 h-60 sm:w-64 sm:h-64 mx-auto my-auto mb-7' >
+                    <h1  className='bg-black text-lime-500 text-center'>snake Game 2.0</h1>            
+                </div>
+            :
+             <ScreenInit renderGame={renderGame} setRenderGame={setRenderGame}/> 
+            }
         </div>
         <div className='w-5/6 m-auto pt-4'>
             <h3 className='font-bold  text-blue-700 '> <span className='text-lg'> Nintendo </span><span className='text-2xl' style={{fontFamily:"Orbitron"}}>GAME BOY</span><span className='text-xs'>TM</span></h3>
@@ -20,9 +34,6 @@ export default function NintendoConsole() {
                 <button  className=' absolute right-2 bottom-14 rotate-90  bg-black w-11 h-14 flex items-center justify-center rounded-t-2xl'><AiOutlineArrowUp color='white'/></button>
                 <button  className=' absolute left-3 bottom-14 -rotate-90  bg-black w-11 h-14 flex items-center justify-center rounded-t-2xl'><AiOutlineArrowUp color='white'/></button>
                 <button  className=' absolute right-14 bottom-2 rotate-180  bg-black w-11 h-14 flex items-center justify-center rounded-t-2xl'><AiOutlineArrowUp color='white'/></button>                
-                {/* <button><AiOutlineArrowRight/></button>
-                <button><AiOutlineArrowLeft/></button>
-                <button><AiOutlineArrowDown/></button> */}
             </div>
             <div className='mr-2 w-36 h-20 bg-slate-200 origin-center -rotate-45 rounded-full  flex items-center justify-evenly'>
                 <button className=' bg-red-900 w-14 h-14 border-2 border-gray-900 rounded-full font-bold text-white'>B</button>
@@ -31,11 +42,11 @@ export default function NintendoConsole() {
         </div>
         <div className='flex items-center justify-center mt-6 gap-7'>
             <div className='h-10 w-14 origin-center -rotate-45'>
-                <div onClick={()=>  sound.play()} className='h-4 bg-slate-700 rounded-full  '></div>
+                <div onClick={()=> {sound.play(),setRenderInitGame(!renderInitGame),setRenderGame(false)}} className='h-4 bg-slate-700 rounded-full  '></div>
                 <h2 className='font-semibold'>SELECT</h2>
             </div>
             <div className='h-10 w-14 origin-center -rotate-45'>
-                <div onClick={()=>  sound.play()} className='h-4 bg-slate-700 rounded-full  ' ></div>
+                <div onClick={start} className='h-4 bg-slate-700 rounded-full  ' ></div>
                 <h2 className='font-semibold'>START</h2>
             </div>
         </div>
---
+++ src/components/ScreenInit/ScreenInit.tsx
@@ -0,0 +1,23 @@
+import sonidoIntro from '../../assets/sonidoIntro.mp3'
+import {prop} from '../../types/types'
+import GameSnake from '../GameSnake/GameSnake'
+export default function ScreenInit({renderGame,setRenderGame}:prop) {
+    if(!renderGame){
+        return (    
+            <div  className='bg-lime-500 w-60 h-60 sm:w-64 sm:h-64 mx-auto my-auto mb-7 flex flex-col items-center justify-center gap-3'>
+               <audio autoPlay loop>
+                <source src={sonidoIntro}/>
+               </audio>
+               <div className=' w-28 h-20'>
+                 <img className='w-full' src='https://i.pinimg.com/originals/47/04/24/470424ded5acade0464f077f007f269b.png'/>
+               </div>
+                <h1 className='font-bold'>Snake Game 2.0</h1>
+                <h3 className='font-bold animate-bounce' style={{ fontFamily:"Press Start" }}>PRESS START</h3>
+            </div>
+          )
+    }
+    return(
+        <GameSnake/>
+    )
+
+}
